cd0d4531633ba05e446ae316263c483ff91e136e
Shayne Sebro <shaynesebro@gmail.com>
2021-07-19 20:08:30 -0400

    adding hero CTA component

---
+++ src/components/hero-cta/HeroCta.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './HeroCta.scss'
+
+const HeroCta = () => {
+  return (
+    <div className="CTA-container">
+      <h1>CORE by ACCENT</h1>
+      <p>Timeless summer jewelry essentials</p>
+        <button className="shop-button">SHOP THE GOLD EDIT</button>
+        <button className="shop-button">SHOP THE SILVER EDIT</button>
+    </div>
+  )
+}
+
+export default HeroCta
--- src/components/menu-item/HeroImage.tsx
+++ src/components/menu-item/HeroImage.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import HeroCta from '../hero-cta/HeroCta'
 import './HeroImage.scss'
 
 const HeroImage: React.FC = () => {
   return (
     <div className="hero-container">
+      <HeroCta />
       <div className="hero-image-1"/>
       <div className="hero-image-2"/>
     </div>
