b76c273c1fc4d43bfd9b00fca262963d3c2c3472
Shayne Sebro <shaynesebro@gmail.com>
2021-07-24 14:27:13 -0400

    more responsive sizing on homepage preview components

--- src/components/homepage-preview/HomepagePreview.tsx
+++ src/components/homepage-preview/HomepagePreview.tsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import HomepagePreviewItem from '../homepage-preview-item/HomepagePreviewItem'
-import './Homepage.scss'
+import './HomepagePreview.scss'
 
 const HomepagePreview = () => {
   return (
     <div className="homepage-preview">
-      <h3 className="homepage-preview-message"></h3>
+      <h3 className="homepage-preview-message">
+      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+      </h3>
       <div className="homepage-preview-list">
         <HomepagePreviewItem />
+        <HomepagePreviewItem />
+        <HomepagePreviewItem />
+      </div>
+      <div>
+        <h3>Explore Accent</h3>
+        <button>Women</button>
+        <button>Men</button>
+        <button>Edit</button>
+        <p>VISIT US IN PERSON. <a href="/">STORE LOCATOR</a></p>
       </div>
     </div>
   )
--- src/pages/hompage/Homepage.tsx
+++ src/pages/hompage/Homepage.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import HomepagePreview from '../../components/homepage-preview/HomepagePreview'
 import HeroImage from '../../components/menu-item/HeroImage'
-import NavBar from '../../components/nav-bar/NavBar'
-import PromoBanner from '../../components/promo-banner/PromoBanner'
 import './Homepage.scss'
 
 const Homepage: React.FC = () => {
