bf02be499f7180c0fd75fc6cbd4b4a1e91c4fdfb
Shayne Sebro <shaynesebro@gmail.com>
2021-07-26 21:22:04 -0400

    routing to product detail component and starting to create layout

--- src/pages/product-detail/ProductDetail.tsx
+++ src/pages/product-detail/ProductDetail.tsx
@@ -3,18 +3,21 @@ import './ProductDetail.scss'
 
 const ProductDetail = () => {
   return (
-    <div className='product-detail'>
-      <div className='product-images'>
-        <div className='product-detail-images'>
-          <div className='detail-image'>SMALL IMAGE HERE</div>
-          <div className='detail-image'>SMALL IMAGE HERE</div>
-          <div className='detail-image'>SMALL IMAGE HERE</div>
+    <div className='temp-container'>
+      <div className='product-detail'>
+        <div className='product-images'>
+          <div className='product-detail-images'>
+            <div className='detail-image'>SMALL IMAGE HERE</div>
+            <div className='detail-image' id='middle-detail-image'>SMALL IMAGE HERE</div>
+            <div className='detail-image'>SMALL IMAGE HERE</div>
+          </div>
+          <div className='product-spolight-images'>
+            <div className='spotlight-image'>LARGE IMAGE HERE</div>
+          </div>
         </div>
-        <div className='product-image-large'>
-          <div className='product-image-main'>LARGE IMAGE HERE</div>
-        </div>
+        <div className='product-info'></div>
       </div>
-      <div className='product-info'></div>
+      <div className='product-description'>PRODUCT DESCRIPTION</div>
     </div>
   )
 }
